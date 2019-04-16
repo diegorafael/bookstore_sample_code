b4f5620dcef8754805ac7fec4335770a2c228fb6
Diego Rafael Oliveira Souza <diegorafaelsouza@gmail.com>
2019-04-16 00:01:25 -0300

    Add files via upload

---
+++ BookstoreApiClient/Models/ItemPrice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookstoreApiClient.Models
+{
+    public class ItemPrice
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ BookstoreApiClient/Models/StockMovement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookstoreApiClient.Models
+{
+    public class StockMovement
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public DateTime MovementDate { get; set; }
+    }
+}
